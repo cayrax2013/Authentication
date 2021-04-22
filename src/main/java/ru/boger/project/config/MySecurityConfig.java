bb6621725ddbabac1a64b5968e51918a7fcb39bf
out-boger-va <out-boger-va>
2021-04-22 13:02:12 +0300

    Deleted th extra code

--- src/main/java/ru/boger/project/config/MySecurityConfig.java
+++ src/main/java/ru/boger/project/config/MySecurityConfig.java
@@ -5,7 +5,6 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
 
 import javax.sql.DataSource;
 
@@ -22,26 +21,11 @@ public class MySecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/hr_info").hasRole("HR")
                 .antMatchers("/manager_info/**").hasRole("MANAGER")
                 .and().formLogin().permitAll();
+
     }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-
         auth.jdbcAuthentication().dataSource(dataSource);
-//        User.UserBuilder userBuilder = User.withDefaultPasswordEncoder();
-//
-//        auth.inMemoryAuthentication()
-//                .withUser(userBuilder
-//                        .username("Polina")
-//                        .password("Polina")
-//                        .roles("MANAGER"))
-//                .withUser(userBuilder
-//                        .username("Vlad")
-//                        .password("Vlad")
-//                        .roles("HR"))
-//                .withUser(userBuilder
-//                        .username("Vanya")
-//                        .password("Vanya")
-//                        .roles("EMPLOYEE"));
     }
 }
